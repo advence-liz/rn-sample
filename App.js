7fc8fef695630d5a970a1c54b60025150fb65a46
advence-liz <advence_liz@163.com>
2019-12-16 14:33:25 +0800

    upd: add iconfont

--- App.js
+++ App.js
@@ -8,7 +8,7 @@
 
 import React, {Component} from 'react';
 import {Platform, StyleSheet, Text, View} from 'react-native';
-
+import Icon from './src/iconfont/Icon';
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
   android:
@@ -24,6 +24,9 @@ export default class App extends Component<Props> {
         <Text style={styles.welcome}>Welcome to React Native!</Text>
         <Text style={styles.instructions}>To get started, edit App.js</Text>
         <Text style={styles.instructions}>{instructions}</Text>
+        <Icon name="arrow_under" ></Icon>
+        <Icon name="arrow_on" ></Icon>
+
       </View>
     );
   }
